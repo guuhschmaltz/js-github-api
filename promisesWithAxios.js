ba1bc8c5906f739e75c7455ef49882cba050070d
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2020-09-01 00:03:59 -0300

    utilizado axios para consumir a api

---
+++ promisesWithAxios.js
@@ -0,0 +1,8 @@
+axios.get('https://api.github.com/users/guuhschmaltz')
+    .then(function(response) {
+        console.log(response);
+        //console.log(response.data.avatar_url);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
